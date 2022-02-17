6f54cefbd5eda869fc161bdf354eb3c77a3457a6
unknown <derek.slauson5@gmail.com>
2022-02-16 20:15:14 -0600

    Basic server is set up

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+require("dotenv").config()
+
+const port = process.env.PORT || 3000
+
+app.use(cors())
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
